15f4b12c59f7d7b478860626befad96f36a4f7f1
KentoDecem <10kento10@gmail.com>
2024-06-02 19:43:17 +0200

    add: translations in different json files

--- src/i18n.js
+++ src/i18n.js
@@ -1,33 +1,23 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import HttpApi from "i18next-http-backend";
 
 // Sprawdź zapisany język w localStorage
 const savedLanguage =
   localStorage.getItem("language") || navigator.language.split("-")[0] || "pl";
 
-i18n.use(initReactI18next).init({
-  resources: {
-    en: {
-      translation: {
-        Welcome: "Welcome to my portfolio",
-        Description: "I am testing if description working corectly!",
-        // inne tłumaczenia
-      },
+i18n
+  .use(initReactI18next)
+  .use(HttpApi) // Użyj backendu HTTP do ładowania plików tłumaczeń
+  .init({
+    fallbackLng: "pl",
+    lng: savedLanguage,
+    backend: {
+      loadPath: "/muslewski/locales/{{lng}}/translation.json", //! Do zmiany /muslewski
     },
-    pl: {
-      translation: {
-        Welcome: "Witaj w moim portfolio",
-        Description: "Testuje czy opis działa poprawnie!",
-        // inne tłumaczenia
-      },
+    interpolation: {
+      escapeValue: false,
     },
-  },
-  lng: savedLanguage, // domyślny język
-  fallbackLng: "pl",
-
-  interpolation: {
-    escapeValue: false,
-  },
-});
+  });
 
 export default i18n;
