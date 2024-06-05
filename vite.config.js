dd12c7b8852cd19bb6d1ef3269296dd83b8c35d0
KentoDecem <10kento10@gmail.com>
2024-06-05 21:17:34 +0200

    add domain: muslewski.com

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/muslewski/",
+  base: "/",
   server: {
     host: "0.0.0.0", // Nasłuchuj na wszystkich interfejsach
     port: 3000, // Możesz ustawić dowolny port, np. 3000
